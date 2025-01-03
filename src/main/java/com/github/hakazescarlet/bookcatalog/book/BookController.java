f415114b94569fd2bcfa93076c5c4f554c661276
HakazeScarlet <frost.scarlet.163@gmail.com>
2025-01-03 19:17:58 +0300

    Created endpoint for getting book by id

--- src/main/java/com/github/hakazescarlet/bookcatalog/book/BookController.java
+++ src/main/java/com/github/hakazescarlet/bookcatalog/book/BookController.java
@@ -1,9 +1,6 @@
 package com.github.hakazescarlet.bookcatalog.book;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +11,11 @@ public class BookController {
     public List<BookSimpleDto> getAllBooks() {
         List<BookSimpleDto> bookSimpleDtos = null;
         return bookSimpleDtos;
+    }
+
+    @GetMapping("/books/{bookId}")
+    public BookDto getBookById(@PathVariable Long bookId) {
+        return new BookDto();
     }
 
     @PostMapping("/books")
---
+++ src/main/java/com/github/hakazescarlet/bookcatalog/book/BookDto.java
@@ -0,0 +1,54 @@
+package com.github.hakazescarlet.bookcatalog.book;
+
+import com.github.hakazescarlet.bookcatalog.author.Author;
+
+import java.time.LocalDate;
+
+public class BookDto {
+
+    private Long id;
+    private String title;
+    private String ISBN;
+    private Author author;
+    private LocalDate publicationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+}
