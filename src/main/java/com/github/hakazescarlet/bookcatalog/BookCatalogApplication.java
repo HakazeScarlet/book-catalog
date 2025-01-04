54b6595ffd3d655680d6aeb02b12efc7cdebb8e3
HakazeScarlet <frost.scarlet.163@gmail.com>
2025-01-04 18:03:56 +0300

    Added swagger & added BookDto implementation from BookSimpleDto

--- src/main/java/com/github/hakazescarlet/bookcatalog/BookCatalogApplication.java
+++ src/main/java/com/github/hakazescarlet/bookcatalog/BookCatalogApplication.java
@@ -2,12 +2,15 @@ package com.github.hakazescarlet.bookcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class BookCatalogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookCatalogApplication.class, args);
     }
-
 }
--- src/main/java/com/github/hakazescarlet/bookcatalog/book/BookDto.java
+++ src/main/java/com/github/hakazescarlet/bookcatalog/book/BookDto.java
@@ -2,39 +2,9 @@ package com.github.hakazescarlet.bookcatalog.book;
 
 import com.github.hakazescarlet.bookcatalog.author.Author;
 
-import java.time.LocalDate;
+public class BookDto extends BookSimpleDto{
 
-public class BookDto {
-
-    private Long id;
-    private String title;
-    private String isbn;
     private Author author;
-    private LocalDate publicationDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
 
     public Author getAuthor() {
         return author;
@@ -42,13 +12,5 @@ public class BookDto {
 
     public void setAuthor(Author author) {
         this.author = author;
-    }
-
-    public LocalDate getPublicationDate() {
-        return publicationDate;
-    }
-
-    public void setPublicationDate(LocalDate publicationDate) {
-        this.publicationDate = publicationDate;
     }
 }
