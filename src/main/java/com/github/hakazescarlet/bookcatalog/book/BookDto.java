0b610625368ba8efa7981781ddb86f78907d3b6c
HakazeScarlet <frost.scarlet.163@gmail.com>
2025-01-19 04:47:02 +0300

    Added list of authors

--- src/main/java/com/github/hakazescarlet/bookcatalog/book/BookDto.java
+++ src/main/java/com/github/hakazescarlet/bookcatalog/book/BookDto.java
@@ -2,15 +2,17 @@ package com.github.hakazescarlet.bookcatalog.book;
 
 import com.github.hakazescarlet.bookcatalog.author.Author;
 
+import java.util.List;
+
 public class BookDto extends BookSimpleDto {
 
-    private Author author;
+    private List<Author> authors;
 
-    public Author getAuthor() {
-        return author;
+    public List<Author> getAuthors() {
+        return authors;
     }
 
-    public void setAuthor(Author author) {
-        this.author = author;
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
     }
 }
